4824f06a37c693fa641f20a04800fe5127027431
Sachi854 <a2211501@sendai-nct.jp>
2023-05-30 20:14:21 +0900

    :tada: py env の reinstall ができるようになった

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,6 +19,18 @@ if [ "$1" = "init" ]; then
 fi
 
 cd /workspace/stable-diffusion-webui
+
+if [ "$1" = "reinstall" ]; then
+    rm -rf venv
+    python3.10 -m venv venv
+    source venv/bin/activate
+    python -m pip install --upgrade pip wheel
+    # requirements_versions.txt の内容をインストール
+    # torch 2.0 を先に入れる
+    pip install clean-fid numba numpy torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+    pip install -r requirements_versions.txt
+fi
+
 mkdir -p /workspace/stable-diffusion-webui/tfchache
 # 起動オプション
 export TORCH_COMMAND="pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118"
