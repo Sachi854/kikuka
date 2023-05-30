5d4e6ba44977fd15d269c0a16c9e472c52b71c2d
Sachi854 <a2211501@sendai-nct.jp>
2023-05-30 19:21:17 +0900

    :bug: cudaのバージョンを修正

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM nvidia/cuda:12.1.1-runtime-ubuntu22.04
+FROM nvidia/cuda:11.8.0-devel-ubuntu22.04
 WORKDIR /workspace
 
 ENV TZ=Asia/Tokyo
@@ -6,6 +6,7 @@ ARG DEBIAN_FRONTEND=noninteractive
 
 RUN apt-get update && \
     apt-get install -y tzdata && \
+    apt-get update && \
     apt-get install -y libglib2.0-0 python3.10-full python3.10-venv python3-pip git wget curl ffmpeg libsm6 unzip && \
     apt-get clean -y && \
     rm -rf /var/lib/apt/lists/*
--- entrypoint.sh
+++ entrypoint.sh
@@ -14,14 +14,14 @@ if [ "$1" = "init" ]; then
     python -m pip install --upgrade pip wheel
     # requirements_versions.txt の内容をインストール
     # torch 2.0 を先に入れる
-    pip install clean-fid numba numpy torch==2.0.0+cu118 torchvision --extra-index-url https://download.pytorch.org/whl/cu118
+    pip install clean-fid numba numpy torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
     pip install -r requirements_versions.txt
 fi
 
 cd /workspace/stable-diffusion-webui
 mkdir -p /workspace/stable-diffusion-webui/tfchache
 # 起動オプション
-export TORCH_COMMAND="pip install torch==2.0.0 torchvision --extra-index-url https://download.pytorch.org/whl/cu118"
+export TORCH_COMMAND="pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118"
 export REQS_FILE="requirements_versions.txt"
 export COMMANDLINE_ARGS="--deepdanbooru --theme dark --opt-channelslast --opt-sdp-no-mem-attention"
 export TRANSFORMERS_CACHE="/workspace/stable-diffusion-webui/tfchache"
