fa7e0cf42079c37187d3075d3696a72a7fa33c32
Sachi854 <a2211501@sendai-nct.jp>
2023-08-12 06:59:40 +0900

    :heavy_plus_sign: add test_downloader.py

---
+++ test_downloader.py
@@ -0,0 +1,18 @@
+import downloader as d
+import unittest
+import urllib.parse
+import urllib.request
+
+
+class TestDownloader(unittest.TestCase):
+    # test get_civitai_api function
+    def test_get_civitai_api(self):
+        self.assertEqual(d.get_civitai_api(
+            "https://civitai.com/models/16014/")["id"], 16014)
+        self.assertRaises(urllib.error.HTTPError, d.get_civitai_api,
+                          "https://www.google.co.jp")
+
+
+if __name__ == '__main__':
+    print("Running tests for downloader.py")
+    unittest.main()
