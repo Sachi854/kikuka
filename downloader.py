846627261eb9ba02d822b5832bf14362982d5b6a
Sachi854 <a2211501@sendai-nct.jp>
2023-08-14 09:18:57 +0900

    :recycle: downloaderのリファクタリングとテストの追加

--- downloader.py
+++ downloader.py
@@ -6,150 +6,151 @@ import re
 import urllib.parse
 import urllib.request
 
-# extensions
 
-# define the get api function
-# this function will get the api
-# arguments: url
-# return: json
+def get_download_site_dict(json_data: dict) -> dict:
+    download_site_dict = {}
+    for i in json_data:
+        # get domain name from url of i
+        domain_name = urllib.parse.urlparse(i["url"]).netloc
+        # if the domain name is not in the download site dict, add it
+        if domain_name not in download_site_dict:
+            download_site_dict[domain_name] = []
+        # append i to the download site dict value list with the domain name
+        download_site_dict[domain_name].append(i)
+
+    # return the download site dict
+    return download_site_dict
 
 
-def get_civitai_api(url):
-    endpoint_url = "https://civitai.com/api/v1/models/" + url.split('/')[-2]
+def get_repo_name(url: str) -> str:
+    # get the last part of the url
+    last_part = url.split('/')[-1]
+    # remove the .git suffix
+    repo_name = last_part.replace('.git', '')
+    # return the repo name
+    return repo_name
+
+
+def generate_git_clone_path_dict(download_base_path_dict: dict, download_git_dict: dict) -> dict:
+    cwd = os.getcwd() + '/'
+    path = cwd + download_base_path_dict[download_git_dict["type"]
+                                         ] + get_repo_name(download_git_dict["url"])
+    return {"path": path, "url": download_git_dict["url"]}
+
+
+def generate_curl_path_dict(download_base_path_dict: dict, download_curl_dict: dict) -> dict:
+    cwd = os.getcwd() + '/'
+    path = cwd + download_base_path_dict[download_curl_dict["type"]
+                                         ] + download_curl_dict["url"].split('/')[-1]
+    return {"path": path, "url": download_curl_dict["url"]}
+
+
+def parse_civitai_url(url: str) -> dict:
+    endpoint_url = "https://civitai.com/api/v1/models/"
+    path = urllib.parse.urlparse(url).path
+    path_list = path.split("/")
+    number_list = [x for x in path_list if x.isdigit()]
     user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
     headers = {'User-Agent': user_agent}
-    req = urllib.request.Request(endpoint_url, headers=headers)
+    req = urllib.request.Request(
+        endpoint_url + number_list[-1], headers=headers)
     with urllib.request.urlopen(req) as response:
         the_page = response.read().decode("utf-8")
     return json.loads(the_page)
 
 
-# define the get file name from url function
-# this function will get the file name from the url
-# arguments: url
-# return: file name
-def get_file_name_from_url(url):
-    return url.split('/')[-1]
+def generate_civitai_path_list(download_base_path_dict: dict, download_civitai_dict: dict) -> list:
+    result = []
+    cwd = os.getcwd() + '/'
+    civitai_json = parse_civitai_url(download_civitai_dict["url"])
+    for i in civitai_json["modelVersions"][0]["files"]:
+        if i["type"] == "VAE":
+            path = cwd + download_base_path_dict["VAE"] + i["name"]
+            result.append({"path": path, "url": i["downloadUrl"]})
+        else:
+            path = cwd + \
+                download_base_path_dict[civitai_json["type"]] + i["name"]
+            result.append({"path": path, "url": i["downloadUrl"]})
+    return result
 
 
-# define the drop file extension function
-# this function will drop the file extension
-# arguments: file name
-# return: file name
-def drop_file_extension(file_name):
-    return file_name.split('.')[0]
+def generate_download_path_dict(download_base_path_dict: dict, download_site_dict: dict) -> dict:
+    result = {"git": [], "curl": []}
+    for i in download_site_dict['github.com']:
+        result["git"].append(
+            generate_git_clone_path_dict(download_base_path_dict=download_base_path_dict, download_git_dict=i))
+    for i in download_site_dict['huggingface.co']:
+        result["curl"].append(generate_curl_path_dict(
+            download_base_path_dict=download_base_path_dict, download_curl_dict=i))
+    for i in download_site_dict['civitai.com']:
+        result["curl"].extend(generate_civitai_path_list(
+            download_base_path_dict=download_base_path_dict, download_civitai_dict=i))
+    return result
 
 
-# define the get repository name function
-# this function will get the repository name from the url
-# arguments: url
-# return: repository name
-def get_repository_name(url):
-    # if url have .git
-    if '.git' in url:
-        return drop_file_extension(get_file_name_from_url(url))
-    else:
-        return get_file_name_from_url(url)
-
-
-# define the async git clone function
-# this function will clone a git repository
-# arguments: file path, url
-# return: none
-async def async_git_clone(file_path, url):
+async def async_git_clone(path: str, url: str) -> None:
     print(url)
     # create a command
     command = ["git", "clone", "--depth=1", url,
-               file_path + get_repository_name(url)]
+               path]
     # create a process
     process = await asyncio.create_subprocess_exec(*command)
     # wait for the process to complete
     await process.wait()
 
 
-# define the async download function
-# this function will download a file using curl
-# arguments: file path, url
-# return: none
-async def async_download(file_path, url):
+async def async_curl(path: str, url: str) -> None:
     print(url)
     # create a command
-    command = ['curl', '-Lo', file_path, url]
+    command = ['curl', '-Lo', path, url]
     # create a process
     process = await asyncio.create_subprocess_exec(*command)
     # wait for the process to complete
     await process.wait()
 
 
-async def main():
-    # get the home directory
-    working_dir = os.getcwd() + '/'
+async def async_download(download_path_dict: dict) -> None:
+    tasks = []
+    for i in download_path_dict["git"]:
+        if not os.path.exists(i["path"]):
+            tasks.append(asyncio.create_task(
+                async_git_clone(path=i["path"], url=i["url"])))
+    for i in download_path_dict["curl"]:
+        if not os.path.exists(i["path"]):
+            tasks.append(asyncio.create_task(
+                async_curl(path=i["path"], url=i["url"])))
+    await asyncio.gather(*tasks)
 
+
+def main():
+    cwd = os.getcwd() + '/'
     if not sys.stdin.isatty():
         # read the standard input as json
         json_data = json.loads(sys.stdin.read())
     # not exists the standard input
     else:
-        # if exists the command line argument, get the command line argument
-        # command line arguments : -f <file path>
-        # if arguments are not set, throw an error
-        try:
-            file_path = sys.argv[sys.argv.index('-f') + 1]
-            # opne the file
-            with open(file_path, 'r') as f:
-                # read the file as json
-                json_data = json.loads(f.read())
-        except ValueError:
-            print('Please set the command line arguments')
-            print('Usage: python file_fetcher.py -f json_file_path')
-            exit(1)
+        print("Please input the json data.")
+        exit(1)
 
-    print(working_dir)
-
-    # mkdir the directory
-    print("mkdir")
-    for i in json_data["directory"]:
-        if i == "ControlNet":
-            continue
-        elif not os.path.exists(i):
+    # mkdir if not exists
+    download_base_path_dict = json_data["path"]
+    for i in download_base_path_dict.values():
+        if not os.path.exists(i):
             os.makedirs(
-                working_dir + json_data["directory"][i], exist_ok=True)
-        else:
-            print("Directory already exists!!")
+                cwd + i, exist_ok=True)
 
-    # download the controlnet files
-    print("download files")
-    tasks = []
-    for i in json_data["url"]:
-        # get domain name from url
-        domain_name = urllib.parse.urlparse(i["url"]).netloc
-        if domain_name == "github.com":
-            file_path = working_dir + json_data["directory"][i["type"]]
-            file_path_clone = file_path + get_repository_name(i["url"])
-            if not os.path.exists(file_path_clone):
-                tasks.append(asyncio.create_task(async_git_clone(
-                    file_path, i["url"])))
-        elif domain_name == "civitai.com":
-            j = get_civitai_api(i["url"])
-            file_path = working_dir + \
-                json_data["directory"][j["type"]] + \
-                j["modelVersions"][0]["files"][0]["name"]
-            if not os.path.exists(file_path):
-                tasks.append(asyncio.create_task(async_download(
-                    file_path, j["modelVersions"][0]["files"][0]["downloadUrl"])))
-        elif domain_name == "huggingface.co":
-            file_path = working_dir + \
-                json_data["directory"][i["type"]] + \
-                get_file_name_from_url(i["url"])
-            if not os.path.exists(file_path):
-                tasks.append(asyncio.create_task(async_download(
-                    file_path, i["url"])))
-        else:
-            print("Unknown site")
-            exit(1)
-    # download the files asynchronously
-    await asyncio.gather(*tasks)
+    # genereate download site dict
+    download_site_dict = get_download_site_dict(json_data=json_data["url"])
+    download_site_dict = generate_download_path_dict(
+        download_base_path_dict=download_base_path_dict, download_site_dict=download_site_dict)
+
+    # async download
+    print("start download")
+    asyncio.run(async_download(download_path_dict=download_site_dict))
+
+    # print the result
+    print("finish download")
+
 
 if __name__ == '__main__':
-    asyncio.run(main())
-    print("Done!!")
+    main()
--- test_downloader.py
+++ test_downloader.py
@@ -2,34 +2,86 @@ import downloader as d
 import unittest
 import urllib.parse
 import urllib.request
+import os
 
 
 class TestDownloader(unittest.TestCase):
-    # test get_civitai_api function
-    def test_get_civitai_api(self):
-        self.assertEqual(d.get_civitai_api(
-            "https://civitai.com/models/16014/")["id"], 16014)
-        self.assertRaises(urllib.error.HTTPError, d.get_civitai_api,
-                          "https://www.google.co.jp")
+    def test_get_download_site_dict(self):
+        site = [{"url": "https://huggingface.co/Akumetsu971/SD_Anime_Futuristic_Armor/resolve/main/4x-AnimeSharp.pth", "type": "UpscaleModel"},
+                {"url": "https://huggingface.co/datasets/gsdf/EasyNegative/resolve/main/EasyNegative.safetensors",
+                    "type": "TextualInversion"},
+                {"url": "https://github.com/Sachi854/kukuri.git", "type": "output"},
+                {"url": "https://civitai.com/models/9828/faruzan-genshin-impact"}]
+        result = {'huggingface.co': [{"url": "https://huggingface.co/Akumetsu971/SD_Anime_Futuristic_Armor/resolve/main/4x-AnimeSharp.pth", "type": "UpscaleModel"},
+                                     {"url": "https://huggingface.co/datasets/gsdf/EasyNegative/resolve/main/EasyNegative.safetensors", "type": "TextualInversion"}],
+                  'github.com': [{"url": "https://github.com/Sachi854/kukuri.git", "type": "output"}],
+                  'civitai.com': [{"url": "https://civitai.com/models/9828/faruzan-genshin-impact"}]}
+        self.assertEqual(d.get_download_site_dict(site), result)
+        site.append(
+            {"urll": "https://civitai.com/models/9828/faruzan-genshin-impact"})
+        self.assertRaises(KeyError, d.get_download_site_dict, site)
 
-    def test_get_file_name_from_url(self):
-        self.assertEqual(d.get_file_name_from_url(
-            "https://civitai.com/models/16014/"), "")
-        self.assertEqual(d.get_file_name_from_url(
-            "https://civitai.com/models/16014/16014/tnk.pdf"), "tnk.pdf")
+    def test_get_repo_name(self):
+        self.assertEqual(d.get_repo_name(
+            "https://github.com/Sachi854/kukuri.git"), "kukuri")
+        self.assertEqual(d.get_repo_name(
+            "https://github.com/Sachi854/kukuri"), "kukuri")
 
-    def test_drop_file_extension(self):
-        self.assertEqual(d.drop_file_extension("tnk.pdf"), "tnk")
-        self.assertEqual(d.drop_file_extension("tnk"), "tnk")
+    def test_generate_git_clone_path_dict(self):
+        cwd = os.getcwd() + '/'
+        self.assertEqual(d.generate_git_clone_path_dict({"output": "output/"}, {
+                         "url": "https://github.com/Sachi854/kukuri.git", "type": "output"}), {"path": cwd+"output/kukuri", "url": "https://github.com/Sachi854/kukuri.git"})
+        self.assertEqual(d.generate_git_clone_path_dict({"output": "output/"}, {
+                         "url": "https://github.com/Sachi854/kukuri", "type": "output"}), {"path": cwd+"output/kukuri", "url": "https://github.com/Sachi854/kukuri"})
 
-    def test_get_repository_name(self):
-        self.assertEqual(d.get_repository_name(
-            "https://github.com/hako-mikan/sd-webui-lora-block-weight.git"), "sd-webui-lora-block-weight")
-        self.assertEqual(d.get_repository_name(
-            "https://github.com/hako-mikan/sd-webui-lora-block-weight"), "sd-webui-lora-block-weight")
-        self.assertEqual(d.get_repository_name(
-            "https://www.google.co.jp"), "www.google.co.jp")
+    def test_generate_curl_path_dict(self):
+        cwd = os.getcwd() + '/'
+        self.assertEqual(d.generate_curl_path_dict({"output": "output/"}, {"url": "https://huggingface.co/Akumetsu971/SD_Anime_Futuristic_Armor/resolve/main/4x-AnimeSharp.pth", "type": "output"}), {
+                         "path": cwd+"output/4x-AnimeSharp.pth", "url": "https://huggingface.co/Akumetsu971/SD_Anime_Futuristic_Armor/resolve/main/4x-AnimeSharp.pth"})
+
+    def test_parse_civitai_url(self):
+        self.assertRaises(IndexError, d.parse_civitai_url,
+                          "https://github.com/Sachi854/kukuri.git")
+
+    def test_generate_civitai_path_list(self):
+        cwd = os.getcwd() + '/'
+        self.assertEqual(d.generate_civitai_path_list({"Checkpoint": "ComfyUI/models/checkpoints/",
+                                                       "VAE": "ComfyUI/models/vae/", }, {"url": "https://civitai.com/models/122533"}), [
+            {"path": cwd+"ComfyUI/models/checkpoints/animagineXL_v10.safetensors",
+                "url": "https://civitai.com/api/download/models/133462"},
+            {"path": cwd+"ComfyUI/models/vae/sdxl_vae.safetensors", "url": "https://civitai.com/api/download/models/133462?type=VAE&format=SafeTensor"}])
+
+    def test_generate_download_path_dict(self):
+        cwd = os.getcwd() + '/'
+        self.assertEqual(d.generate_download_path_dict({
+            "Checkpoint": "ComfyUI/models/checkpoints/",
+            "VAE": "ComfyUI/models/vae/",
+            "TextualInversion": "ComfyUI/models/embeddings/",
+            "AestheticGradient": "ComfyUI/models/embeddings/",
+            "Hypernetwork": "ComfyUI/models/hypernetworks/",
+            "LORA": "ComfyUI/models/loras/",
+            "Controlnet": "ComfyUI/models/controlnet/",
+            "UpscaleModel": "ComfyUI/models/upscale_models/",
+            "output": "ComfyUI/output/"},
+            {'github.com': [{
+                "url": "https://github.com/Sachi854/kukuri.git", "type": "output"}],
+             'huggingface.co': [{"url": "https://huggingface.co/Akumetsu971/SD_Anime_Futuristic_Armor/resolve/main/4x-AnimeSharp.pth", "type": "output"}],
+             'civitai.com': [{"url": "https://civitai.com/models/122533"}]}),
+            {
+                "git": [
+                    {"path": cwd+"ComfyUI/output/kukuri",
+                        "url": "https://github.com/Sachi854/kukuri"}
+                ],
+                "curl": [
+                    {"path": cwd+"ComfyUI/models/upscale_models/4x-AnimeSharp.pth",
+                     "url": "https://huggingface.co/Akumetsu971/SD_Anime_Futuristic_Armor/resolve/main/4x-AnimeSharp.pth"},
+                    {"path": cwd+"ComfyUI/models/checkpoints/animagineXL_v10.safetensors",
+                     "url": "https://civitai.com/api/download/models/133462"},
+                    {"path": cwd+"ComfyUI/models/vae/sdxl_vae.safetensors",
+                        "url": "https://civitai.com/api/download/models/133462?type=VAE&format=SafeTensor"}
+                ]
+        })
+
 
 if __name__ == '__main__':
-    print("Running tests for downloader.py")
     unittest.main()
